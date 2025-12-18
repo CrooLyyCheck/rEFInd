0e5022a7a3b147e53426f4fd06ad36fe3c219866
CrooLyyCheck <59777089+CrooLyyCheck@users.noreply.github.com>
2025-12-18 02:59:11 +0100

    del

--- reboot-to-windows-terminal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root/sudo user"
-    exit 1
-fi
-
-cp -f /boot/efi/EFI/refind/PreviousBoot-windows /boot/efi/EFI/refind/vars/PreviousBoot
-reboot
